d168173b7f0d1f010c0625a46395476740135a5c
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-12-01 13:07:24 -0700

    Update file structure

---
+++ data/data.js
@@ -0,0 +1,30 @@
+module.exports = {
+    monsters: [{
+        id: 1,
+        type: "scaly",
+        image_url: "https://cdn.pixabay.com/photo/2016/01/10/14/14/monster-1131847_960_720.png"
+    },
+    {
+        id: 2,
+        type: "slithery",
+        image_url: "https://cdn.pixabay.com/photo/2016/01/10/14/11/monster-1131840_960_720.png"
+    },
+    {
+        id: 3,
+        type: "electric",
+        image_url: "https://cdn.pixabay.com/photo/2016/01/10/14/14/monster-1131843_960_720.png"
+    }],
+    translators: [{
+        id: 1,
+        translator: "backwards"
+    },
+    {
+        id: 2,
+        translator: "remove-vowels"
+    },
+    {
+        id: 3,
+        translator: "hacker-speak"
+    }]
+}
+
