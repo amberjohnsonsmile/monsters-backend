3aa88a4231ef7ab29d16ad90658a2e9ac684937e
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-12-15 14:56:30 -0700

    Refactor functions

--- queries.js
+++ queries.js
@@ -1,19 +1,18 @@
 function monstersPlusTranslators(monsters, translators) {
-    let combined = monsters;
-    for (i = 0; i < combined.length; i++) {
-        for (j = 0; j < translators.length; j++) {
-            if (combined[i].id === translators[j].id) {
-                combined[i].translator = translators[j].translator;
+    monsters.map(monsterItem => {
+        translators.map(translatorItem => {
+            if (monsterItem.id === translatorItem.id) {
+                monsterItem.translator = translatorItem.translator;
             }
-        }
-    }
-    return combined;
+        });
+    });
+    return monsters;
 }
 
 function resetMonsters(monsters) {
-    monsters.map(function(item) {
+    monsters.forEach(item => {
         delete item.translator;
-    })
+    });
 }
 
 module.exports = {
